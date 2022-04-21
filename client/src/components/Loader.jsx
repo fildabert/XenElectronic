31f0a06e27ce0b92cef505b87a227a64765039ea
fildabert <filbert@dkatalis.com>
2022-04-21 10:42:03 +0700

    refactored actions and added loader

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import { Oval } from 'react-loader-spinner';
+
+export default function Loader() {
+  return (
+    <div className="container-row justify-content-center">
+      <Oval color="#00BFFF" height={80} width={80} />
+    </div>
+  )
+}
--- client/src/store/actions/product.action.js
+++ client/src/store/actions/product.action.js
@@ -1,24 +1,20 @@
 import axios from 'axios';
-import { BASE_URL } from '../store.constant';
+import { BASE_URL, EVENTS } from '../store.constant';
 
 export const fetchProducts = () => {
   return async (dispatch) => {
+    dispatch({ type: EVENTS.IS_LOADING, payload: true });
     const response = await axios({
       method: 'GET',
       url: `${BASE_URL}/product`,
     });
-    dispatch(setProducts(response.data));
-  };
-};
-
-export const setProducts = (payload) => {
-  return (dispatch) => {
-    dispatch({ type: 'fetchProducts', payload: payload.data });
+    dispatch({ type: EVENTS.FETCH_PRODUCTS, payload: response.data.data });
+    dispatch({ type: EVENTS.IS_LOADING, payload: false });
   };
 };
 
 export const selectCategory = (category) => {
   return (dispatch) => {
-    dispatch({ type: 'selectCategory', payload: category });
+    dispatch({ type: EVENTS.SELECT_CATEGORY, payload: category });
   };
 };
